e9d5a10cc3b0192856df08bf47d2ef93004c94c1
DESKTOP-R7NQEQM\wayan <wayanarisanjaya01@gmail.com>
2024-08-16 13:40:07 +0800

    initial development

---
+++ resources/views/apps/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('theme.panel')
+
+@section('title', 'Dashboard')
+
+@section('style')
+
+@endsection
+
+@section('content')
+    <!-- Page header -->
+    <div class="page-header d-print-none">
+        <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <!-- Page pre-title -->
+                    <div class="page-pretitle">
+                        Menu
+                    </div>
+                    <h2 class="page-title">
+                        Dashboard
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page body -->
+    <div class="page-body">
+
+    </div>
+@endsection
+
+@section('url')
+
+@endsection
+
+@section('script')
+
+@endsection
---
+++ resources/views/partials/script.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ asset('assets') }}/dist/libs/apexcharts/dist/apexcharts.min.js?1692870487" defer></script>
+<script src="{{ asset('assets') }}/dist/libs/jsvectormap/dist/js/jsvectormap.min.js?1692870487" defer></script>
+<script src="{{ asset('assets') }}/dist/libs/jsvectormap/dist/maps/world.js?1692870487" defer></script>
+<script src="{{ asset('assets') }}/dist/libs/jsvectormap/dist/maps/world-merc.js?1692870487" defer></script>
+<!-- Tabler Core -->
+<script src="{{ asset('assets') }}/dist/js/tabler.min.js?1692870487" defer></script>
+<script src="{{ asset('assets') }}/dist/js/demo.min.js?1692870487" defer></script>
